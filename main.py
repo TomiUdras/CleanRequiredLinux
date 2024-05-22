668cd82c436aa7c5e4893b413bdc6b48075262d3
Artem <90392848+TomiUdras@users.noreply.github.com>
2024-05-22 13:49:41 +0000

    new ver

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 s = None
 my_dict = {}
-while (True):
-  s = input().split(': ')
-  if 'СТОП' in s:
-    break
-  my_dict[s[0]] = s[1].split(', ')
+
+while True:
+    s = input().split(': ')
+    if 'СТОП' in s:
+        break
+    my_dict[s[0]] = [float(value) for value in s[1].split(', ')]
 
 names = input().split(', ')
 for name in names:
-  if name.split('-')[0] in my_dict:
-    values = my_dict[name.split('-')[0]]
-    print(
-        f"Продукт: {name.split('-')[0]}, калорийность: {int(name.split('-')[1])*(int(values[0])*4 + int(values[1])*9 + int(values[2])*4)}"
-    )
+    if name.split('-')[0] in my_dict:
+        values = my_dict[name.split('-')[0]]
+        calories = int(name.split('-')[1]) * (values[0] * 4 + values[1] * 9 + values[2] * 4)
+        print(f"Продукт: {name.split('-')[0]}, калорийность: {calories}")
